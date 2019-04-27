393bbf601d5d59a79ee8755094b2964a09105361
DaianaArantes <daiana.arantes@hotmail.com>
2019-04-27 00:27:47 -0400

    Add camera to app, and save it to the database

---
+++ www/js/DAcamera.js
@@ -0,0 +1,93 @@
+/**
+ * File Name: DAcamera.js
+ * Revision History:
+ * Daiana Arantes, 2019-04-26 : Created
+ */
+function DAcapturePhoto() {
+    var options = {
+        quality : 50,
+        destinationType: Camera.DestinationType.DATA_URL,
+        sourceType: Camera.PictureSourceType.CAMERA,
+        saveToPhotoAlbum: true
+    };
+
+    function successCallback(imageData) {
+        var image = $("#imgFile");
+        image.prop("src", "data:image/jpeg;base64," + imageData);
+        $("#imageData").val("data:image/png;base64," + imageData);
+        $("#imgFile").show();
+    }
+
+    function errorCallback(error) {
+        alert("Error: " + error.message);
+    }
+
+    navigator.camera.getPicture(successCallback, errorCallback, options);
+}
+
+function DAcaptureEditablePhoto() {
+    var options = {
+        quality : 50,
+        allowEdit: true,
+        destinationType: Camera.DestinationType.DATA_URL,
+        sourceType: Camera.PictureSourceType.CAMERA,
+        saveToPhotoAlbum: true
+    };
+
+    function successCallback(imageData) {
+        var image = $("#imgFileEdit");
+        image.prop("src", "data:image/jpeg;base64," + imageData);
+        $("#imageDataEdit").val("data:image/png;base64," + imageData);
+        $("#imgFileEdit").show();
+    }
+
+    function errorCallback(error) {
+        alert("Error: " + error.message);
+    }
+
+    navigator.camera.getPicture(successCallback, errorCallback, options);
+}
+
+function DAloadFromLibrary() {
+    var options = {
+        quality : 50,
+        allowEdit: true,
+        destinationType: Camera.DestinationType.DATA_URL,
+        sourceType: Camera.PictureSourceType.PHOTOLIBRARY
+    };
+
+    function successCallback(imageData) {
+        var image = $("#imgFile");
+        image.prop("src", "data:image/jpeg;base64," + imageData);
+        $("#imageData").val("data:image/png;base64," + imageData);
+        $("#imgFile").show();
+    }
+
+    function errorCallback(error) {
+        alert("Error: " + error.message);
+    }
+
+    navigator.camera.getPicture(successCallback, errorCallback, options);
+}
+
+function DAloadFromLibraryEdit() {
+    var options = {
+        quality : 50,
+        allowEdit: true,
+        destinationType: Camera.DestinationType.DATA_URL,
+        sourceType: Camera.PictureSourceType.PHOTOLIBRARY
+    };
+
+    function successCallback(imageData) {
+        var image = $("#imgFileEdit");
+        image.prop("src", "data:image/jpeg;base64," + imageData);
+        $("#imageDataEdit").val("data:image/png;base64," + imageData);
+        $("#imgFileEdit").show();
+    }
+
+    function errorCallback(error) {
+        alert("Error: " + error.message);
+    }
+
+    navigator.camera.getPicture(successCallback, errorCallback, options);
+}
