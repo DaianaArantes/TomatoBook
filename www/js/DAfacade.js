906c778caf7bfcb25e4c33fbc162e3b383f6d8fc
DaianaArantes <daiana.arantes@hotmail.com>
2019-04-26 20:19:28 -0400

    Create global and facade file with funtions to the buttons

---
+++ www/js/DAfacade.js
@@ -0,0 +1,197 @@
+/**
+ * File Name: DAfacade.js
+ * Revision History:
+ * Daiana Arantes, 2019-04-27 : Created
+ */
+function DAupdateShapeDropdown() {
+
+    var options = [];
+
+    function callback(tx, results) {
+        var htmlCode = "";
+
+        for (var i = 0; i < results.rows.length; i++) {
+            var row = results.rows[i];
+            if (row['name'] === 'Cocktail'){
+                htmlCode += "<option value=" + row['name'] + " selected>" + row['name'] + "</option>";
+            }else{
+                htmlCode += "<option value=" + row['name'] + ">" + row['name'] + "</option>";
+            }
+        }
+        var select = $("#daAddShape");
+        select = select.html(htmlCode);
+
+        select.selectmenu("refresh");
+    }
+    shape.DAselectAll(options, callback);
+}
+
+function DAaddTomato(){
+
+    if (daValidate_daAddForm())
+    {
+        console.info("Validation is successful");
+        var options;
+        var commonName = $("#daAddTomatoName").val();
+        var scientificName = $("#daAddScientificName").val();
+        var size = $("input[name='daRadSizeAdd']:checked").val();
+        var shapeId = $("#daAddShape").prop("selectedIndex");
+        var colorName = $("#daAddColor").val();
+        var moreInfo = $("#daAddAdditionalInfo").val();
+        var foundDate = $("#daAddDate").val();
+
+        var dataURL = $("#imageData").val();
+        options = [commonName, scientificName, size, shapeId, colorName, moreInfo, foundDate, dataURL];
+
+        function callback()
+        {
+            console.info("Record inserted successfully");
+            $(location).prop('href', "#daViewAllTomatoesPage");
+        }
+
+
+        tomato.DAinsert(options, callback);
+        alert("New Tomato Added");
+    }
+    else
+    {
+        console.error("Validation failed");
+    }
+}
+function DAgetTomatoes(){
+    var options = [];
+
+    function callback(tx, results)
+    {
+        var htmlCode = "";
+
+        for (var i = 0; i < results.rows.length; i++)
+        {
+            var row = results.rows[i];
+
+            htmlCode += "<li data-icon=\"carat-r\">" +
+                "<a data-role='button' data-row-id=" + row['id'] + " href='#'>" +
+                "<h2>Common Name: " + row['commonName'] + "</h2>" +
+                "<p>Scientific Name: " + row['scientificName'] + "</p>" +
+                "<p>Color: " + row['colorName'] + "</p>" +
+                "</a>" +
+                "</li>";
+        }
+
+        var lv = $("#daTomatoList");
+        lv = lv.html(htmlCode);
+
+        lv.listview("refresh");
+
+        function clickHandler()
+        {
+            localStorage.setItem("id", $(this).attr("data-row-id") );
+            $(location).prop('href', '#daEditTomatoPage');
+        }
+        $("#daTomatoList a").on("click", clickHandler);
+    }
+
+    tomato.DAselectAll(options, callback);
+}
+
+function DAshowCurrentTomato (){
+
+    var id = localStorage.getItem("id");
+    var options = [id];
+
+    function callback(tx, results) {
+        var row = results.rows[0];
+
+
+        $("#daEditTomatoName").val(row['commonName']);
+        $("#daEditScientificName").val(row['scientificName']);
+        $("#daEditShape").prop('selectedIndex', row['shapeId']);
+        $("#daEditColor").val(row['colorName']);
+        $("#daEditAdditionalInfo").val(row['moreInfo']);
+        $("#daEditDate").val(row['foundDate']);
+
+        var select = $("#daEditShape");
+        select.selectmenu("refresh");
+
+        if (row['size'] === 'Small') {
+            $("#daRadSmallEdit").prop('checked',true).click();
+
+        }else if (row['size'] === 'Medium') {
+            $("#daRadMediumEdit").prop('checked',true).click();
+        }
+        else{
+            $("#daRadLargeEdit").prop('checked',true).click();
+        }
+        $("#daRadSizeEdit :radio").checkboxradio('refresh');
+
+        var img = document.getElementById("imgFileEdit");
+        img.src = row['photo'];
+        $("#imageDataEdit").val(row['photo']);
+        if ($("#imageDataEdit").val() !== "")  {
+            $("#imgFileEdit").show();
+        }else{
+            $("#imgFileEdit").hide();
+        }
+
+
+    }
+
+        tomato.DAselect(options, callback);
+}
+
+function DAupdateTomato(){
+
+    if (daValidate_daEditForm()) {
+        console.info("Validation is successful");
+
+        var commonName = $("#daEditTomatoName").val();
+        var scientificName = $("#daEditScientificName").val();
+        var size = $("input[name='daRadSizeEdit']:checked").val();
+        var shapeId = $("#daEditShape").prop("selectedIndex");
+        var colorName = $("#daEditColor").val();
+        var moreInfo = $("#daEditAdditionalInfo").val();
+        var foundDate = $("#daEditDate").val();
+        var id= localStorage.getItem("id");
+        var dataURL = $("#imageDataEdit").val();
+        var options;
+
+        options = [commonName, scientificName, size, shapeId, colorName, moreInfo, foundDate, dataURL, id];
+
+        function callback() {
+            console.info("Record updated successfully");
+            $(location).prop('href', "#daViewAllTomatoesPage");
+        }
+
+        alert("Tomato Updated successfully");
+    }else{
+        console.error("Validation failed");
+    }
+    tomato.DAupdate(options, callback);
+}
+
+function DAdeleteTomato(){
+    var id= localStorage.getItem("id");
+    var options = [id];
+
+    function callback() {
+        console.info("Record deleted successfully");
+        $(location).prop('href', "#daViewAllTomatoesPage");
+    }
+    alert("Tomato Deleted successfully");
+    tomato.DAdelete(options, callback);
+}
+//To clear add page
+function DArefreshAdd(){
+    $("#daAddTomatoName").val("");
+    $("#daAddScientificName").val("");
+    $("#daRadSmallAdd").prop('checked',true).click();
+    $("#daAddShape").prop('selectedIndex', 0);
+    $("#daAddColor").val("");
+    $("#daAddAdditionalInfo").val("");
+    $("#daAddDate").val("");
+    $("#imageDataEdit").val("");
+    $("#imgFile").hide();
+    //$("#imgFileEdit").hide();
+    var select = $("#daAddShape");
+    select.selectmenu("refresh");
+}
---
+++ www/js/DAglobal.js
@@ -0,0 +1,94 @@
+/**
+ * File Name: DAglobal.js
+ * Revision History:
+ * Daiana Arantes, 2019-03-27 : Created
+ */
+function daBtnSave_click() {
+    DAaddTomato();
+
+}
+function daBtnUpdate_click() {
+    DAupdateTomato();
+}
+
+function daAddTomatoPage_show() {
+    DAupdateShapeDropdown();
+    DArefreshAdd();
+}
+
+function daViewAllTomatoesPage_show() {
+    DAgetTomatoes();
+}
+
+function daEditTomatoPage_show() {
+    DAshowCurrentTomato();
+}
+
+function daBtnDelete_click() {
+    DAdeleteTomato();
+}
+
+
+function btnCaptureAdd_click() {
+    DAcapturePhoto();
+}
+
+function btnLoadFromLibraryAdd_click() {
+    DAloadFromLibrary();
+}
+
+function btnLoadFromLibraryEdit_click() {
+    DAloadFromLibraryEdit();
+}
+
+function btnCaptureEdit_click() {
+    DAcaptureEditablePhoto();
+}
+
+function daBtnCancel_click() {
+    $(location).prop('href', "#daViewAllTomatoesPage");
+}
+
+function init() {
+
+    $("#daBtnSave").on("click", daBtnSave_click);
+    $("#daBtnUpdate").on("click", daBtnUpdate_click);
+    $("#daBtnDelete").on("click", daBtnDelete_click);
+
+    $("#daAddTomatoPage").on("pageshow", daAddTomatoPage_show);
+    $("#daViewAllTomatoesPage").on("pageshow", daViewAllTomatoesPage_show);
+    $("#daEditTomatoPage").on("pageshow", daEditTomatoPage_show);
+
+    $("#btnCaptureAdd").on("click", btnCaptureAdd_click);
+    $("#btnCaptureEdit").on("click", btnCaptureEdit_click);
+    $("#btnLoadFromLibraryAdd").on("click", btnLoadFromLibraryAdd_click);
+    $("#btnLoadFromLibraryEdit").on("click", btnLoadFromLibraryEdit_click);
+    $("#daBtnCancel").on("click", daBtnCancel_click);
+
+}
+
+$(document).ready(function (){
+
+    init();
+    initDB();
+});
+
+function initDB()
+{
+    try
+    {
+        DB.DACreateDatabase();
+        if (db)
+        {
+            DB.DACreateTables();
+        }
+        else
+        {
+            console.error("Error: Cannot create tables: Database does not exist");
+        }
+    }
+    catch (e)
+    {
+        console.error("Error: (Fatal) Error in initDB(). Cannot proceed");
+    }
+}
